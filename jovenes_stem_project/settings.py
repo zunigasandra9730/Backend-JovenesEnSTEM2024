bbcec5d79af5cd127ac313e549c8e539fe69d279
Cesar Martinez <oliver-cesar@hotmail.com>
2024-11-27 22:38:41 -0600

    CSRF

--- jovenes_stem_project/settings.py
+++ jovenes_stem_project/settings.py
@@ -153,3 +153,5 @@ STORAGES = {
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
 ALLOWED_HOSTS = ['localhost','web-production-71d9.up.railway.app']
+
+CSRF_TRUSTED_ORIGINS = ['http://*','https://web-production-71d9.up.railway.app']
