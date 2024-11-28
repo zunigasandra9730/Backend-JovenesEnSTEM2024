26e8c22e386e5f8bdddf03c953ba5e91892d42ee
Cesar Martinez <oliver-cesar@hotmail.com>
2024-11-28 14:37:38 -0600

    Final Deploy

--- jovenes_stem_project/settings.py
+++ jovenes_stem_project/settings.py
@@ -152,7 +152,7 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 # Autorización CORS
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
-    'https://frontend-jovenesenstem2024-production.up.railway.app', 'https://www.jovenesenstem-ugb.com'
+    'https://frontend-jovenesenstem2024-production.up.railway.app', 'https://www.jovenesenstem-ugb.com','https://jovenesenstem-ugb.com'
     ]
 
 # No mostrar vista de API a usuarios no autenticados
